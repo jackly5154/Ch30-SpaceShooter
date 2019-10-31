5050a4c0087f54068828ef6484bdc65c1b5f17d2
jackly5154 <49957081+jackly5154@users.noreply.github.com>
2019-10-31 16:50:30 -0400

    Created the bounds check script for enemies but didn't delete them off the screen yet

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -4,56 +4,73 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
-
     [Header("Set in Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
+
+    // Start is called before the first frame update
     void Awake()
     {
         camHeight = Camera.main.orthographicSize;
         camWidth = camHeight * Camera.main.aspect;
     }
 
+    // Update is called once per frame
     void LateUpdate()
     {
         Vector3 pos = transform.position;
-        if(pos.x>camWidth - radius)
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false;
+        if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
-        if (pos.x<-camWidth + radius)
+
+        if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
-        if(pos.y > camHeight - radius)
+
+        if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
-        if(pos.y<-camHeight + radius)
+
+        if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
-        transform.position = pos;
+
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false;
+        }
     }
 
-    void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
         if (!Application.isPlaying) return;
-        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, 0.1f);
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, .1f);
         Gizmos.DrawWireCube(Vector3.zero, boundSize);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;
+    public float fireRate = .3f;
+    public float health = 10;
+    public int score = 100;
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void Update()
+    {
+        Move();
+
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.tag == "ProjectileHero")
+        {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
+
+}
